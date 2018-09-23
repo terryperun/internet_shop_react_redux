b907e9d8475100ad0aa501a3edb54054664c49d0
Pikser <32457085+Pikser@users.noreply.github.com>
2018-09-23 14:25:10 +0300

    setup redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
-
+import { Provider } from 'react-redux';
+import store from './store/createStore';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -10,9 +11,11 @@ import registerServiceWorker from './registerServiceWorker';
 import routes from './routes/router';
 
 ReactDOM.render(
-  <App>
-    <Router history={browserHistory} routes={routes} />
-  </App>,
+  <Provider store={store}>
+    <App>
+      <Router history={browserHistory} routes={routes} />
+    </App>
+  </Provider>,
   document.getElementById('root'),
 );
 registerServiceWorker();
